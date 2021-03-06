6d64085766699e882c9a68a17dfb8f79c78f1bfc
air75 <laksariy@yahoo.fr>
2021-03-06 10:11:46 +0100

    modif 6

--- src/main/java/com/alibaba/fastjson/parser/deserializer/NumberDeserializer.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/NumberDeserializer.java
@@ -12,118 +12,119 @@ import com.alibaba.fastjson.util.TypeUtils;
 
 public class NumberDeserializer implements ObjectDeserializer {
 
-    public final static NumberDeserializer instance = new NumberDeserializer();
+	public final static NumberDeserializer instance = new NumberDeserializer();
+	// test
 
-    @SuppressWarnings("unchecked")
-    public <T> T deserialze(DefaultJSONParser parser, Type clazz, Object fieldName) {
-        final JSONLexer lexer = parser.lexer;
-        if (lexer.token() == JSONToken.LITERAL_INT) {
-            if (clazz == double.class || clazz  == Double.class) {
-                String val = lexer.numberString();
-                lexer.nextToken(JSONToken.COMMA);
-                return (T) Double.valueOf(Double.parseDouble(val));
-            }
-            
-            long val = lexer.longValue();
-            lexer.nextToken(JSONToken.COMMA);
+	@SuppressWarnings("unchecked")
+	public <T> T deserialze(DefaultJSONParser parser, Type clazz, Object fieldName) {
+		final JSONLexer lexer = parser.lexer;
+		if (lexer.token() == JSONToken.LITERAL_INT) {
+			if (clazz == double.class || clazz == Double.class) {
+				String val = lexer.numberString();
+				lexer.nextToken(JSONToken.COMMA);
+				return (T) Double.valueOf(Double.parseDouble(val));
+			}
 
-            if (clazz == short.class || clazz == Short.class) {
-                if (val > Short.MAX_VALUE || val < Short.MIN_VALUE) {
-                    throw new JSONException("short overflow : " + val);
-                }
-                return (T) Short.valueOf((short) val);
-            }
+			long val = lexer.longValue();
+			lexer.nextToken(JSONToken.COMMA);
 
-            if (clazz == byte.class || clazz == Byte.class) {
-                if (val > Byte.MAX_VALUE || val < Byte.MIN_VALUE) {
-                    throw new JSONException("short overflow : " + val);
-                }
+			if (clazz == short.class || clazz == Short.class) {
+				if (val > Short.MAX_VALUE || val < Short.MIN_VALUE) {
+					throw new JSONException("short overflow : " + val);
+				}
+				return (T) Short.valueOf((short) val);
+			}
 
-                return (T) Byte.valueOf((byte) val);
-            }
+			if (clazz == byte.class || clazz == Byte.class) {
+				if (val > Byte.MAX_VALUE || val < Byte.MIN_VALUE) {
+					throw new JSONException("short overflow : " + val);
+				}
 
-            if (val >= Integer.MIN_VALUE && val <= Integer.MAX_VALUE) {
-                return (T) Integer.valueOf((int) val);
-            }
-            return (T) Long.valueOf(val);
-        }
+				return (T) Byte.valueOf((byte) val);
+			}
 
-        if (lexer.token() == JSONToken.LITERAL_FLOAT) {
-            if (clazz == double.class || clazz == Double.class) {
-                String val = lexer.numberString();
-                lexer.nextToken(JSONToken.COMMA);
-                return (T) Double.valueOf(Double.parseDouble(val));
-            }
+			if (val >= Integer.MIN_VALUE && val <= Integer.MAX_VALUE) {
+				return (T) Integer.valueOf((int) val);
+			}
+			return (T) Long.valueOf(val);
+		}
 
-            if (clazz == short.class || clazz == Short.class) {
-                BigDecimal val = lexer.decimalValue();
-                lexer.nextToken(JSONToken.COMMA);
-                short shortValue = TypeUtils.shortValue(val);
-                return (T) Short.valueOf(shortValue);
-            }
+		if (lexer.token() == JSONToken.LITERAL_FLOAT) {
+			if (clazz == double.class || clazz == Double.class) {
+				String val = lexer.numberString();
+				lexer.nextToken(JSONToken.COMMA);
+				return (T) Double.valueOf(Double.parseDouble(val));
+			}
 
-            if (clazz == byte.class || clazz == Byte.class) {
-                BigDecimal val = lexer.decimalValue();
-                lexer.nextToken(JSONToken.COMMA);
-                byte byteValue = TypeUtils.byteValue(val);
-                return (T) Byte.valueOf(byteValue);
-            }
+			if (clazz == short.class || clazz == Short.class) {
+				BigDecimal val = lexer.decimalValue();
+				lexer.nextToken(JSONToken.COMMA);
+				short shortValue = TypeUtils.shortValue(val);
+				return (T) Short.valueOf(shortValue);
+			}
 
-            BigDecimal val = lexer.decimalValue();
-            lexer.nextToken(JSONToken.COMMA);
+			if (clazz == byte.class || clazz == Byte.class) {
+				BigDecimal val = lexer.decimalValue();
+				lexer.nextToken(JSONToken.COMMA);
+				byte byteValue = TypeUtils.byteValue(val);
+				return (T) Byte.valueOf(byteValue);
+			}
 
-            if (lexer.isEnabled(Feature.UseBigDecimal)) {
-                return (T) val;
-            } else {
-                return (T) Double.valueOf(val.doubleValue());
-            }
-        }
+			BigDecimal val = lexer.decimalValue();
+			lexer.nextToken(JSONToken.COMMA);
 
-        if (lexer.token() == JSONToken.IDENTIFIER && "NaN".equals(lexer.stringVal())) {
-            lexer.nextToken();
-            Object nan = null;
-            if (clazz == Double.class) {
-                nan = Double.NaN;
-            } else if (clazz == Float.class) {
-                nan = Float.NaN;
-            }
-            return (T) nan;
-        }
+			if (lexer.isEnabled(Feature.UseBigDecimal)) {
+				return (T) val;
+			} else {
+				return (T) Double.valueOf(val.doubleValue());
+			}
+		}
 
-        Object value = parser.parse();
+		if (lexer.token() == JSONToken.IDENTIFIER && "NaN".equals(lexer.stringVal())) {
+			lexer.nextToken();
+			Object nan = null;
+			if (clazz == Double.class) {
+				nan = Double.NaN;
+			} else if (clazz == Float.class) {
+				nan = Float.NaN;
+			}
+			return (T) nan;
+		}
 
-        if (value == null) {
-            return null;
-        }
+		Object value = parser.parse();
 
-        if (clazz == double.class || clazz == Double.class) {
-            try {
-                return (T) TypeUtils.castToDouble(value);
-            } catch (Exception ex) {
-                throw new JSONException("parseDouble error, field : " + fieldName, ex);
-            }
-        }
+		if (value == null) {
+			return null;
+		}
 
-        if (clazz == short.class || clazz == Short.class) {
-            try {
-                return (T) TypeUtils.castToShort(value);
-            } catch (Exception ex) {
-                throw new JSONException("parseShort error, field : " + fieldName, ex);
-            }
-        }
+		if (clazz == double.class || clazz == Double.class) {
+			try {
+				return (T) TypeUtils.castToDouble(value);
+			} catch (Exception ex) {
+				throw new JSONException("parseDouble error, field : " + fieldName, ex);
+			}
+		}
 
-        if (clazz == byte.class || clazz == Byte.class) {
-            try {
-                return (T) TypeUtils.castToByte(value);
-            } catch (Exception ex) {
-                throw new JSONException("parseByte error, field : " + fieldName, ex);
-            }
-        }
+		if (clazz == short.class || clazz == Short.class) {
+			try {
+				return (T) TypeUtils.castToShort(value);
+			} catch (Exception ex) {
+				throw new JSONException("parseShort error, field : " + fieldName, ex);
+			}
+		}
 
-        return (T) TypeUtils.castToBigDecimal(value);
-    }
+		if (clazz == byte.class || clazz == Byte.class) {
+			try {
+				return (T) TypeUtils.castToByte(value);
+			} catch (Exception ex) {
+				throw new JSONException("parseByte error, field : " + fieldName, ex);
+			}
+		}
 
-    public int getFastMatchToken() {
-        return JSONToken.LITERAL_INT;
-    }
+		return (T) TypeUtils.castToBigDecimal(value);
+	}
+
+	public int getFastMatchToken() {
+		return JSONToken.LITERAL_INT;
+	}
 }
